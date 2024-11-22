c6712a0c06bbf188904646ed1629bfcd644ccbfa
souradeep gharami <souradeepgharami2000@gmail.com>
2024-11-22 16:22:20 +0530

    initial home template is ready

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,24 +16,28 @@ export class HomeComponent implements AfterViewInit {
     {
       text: 'This is the first item with an image and paragraph. ahdua jabude ajhdua kajidjie ajhxuah',
     },
-    { text: 'The second item comes from the right.' },
-    { text: 'Here’s another item from the left.' },
+    {
+      text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make',
+    },
+    {
+      text: 'Here’s another item from the left. ypesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    },
     { text: 'And the final item slides in from the right!' },
   ];
 
   ngAfterViewInit() {
-    // Animate each content block
     gsap.utils.toArray('.content').forEach((element: any, index: number) => {
       gsap.fromTo(
         element,
-        { opacity: 0, x: index % 2 === 0 ? -100 : 100 }, // Alternates direction
+        { opacity: 0, x: index % 2 === 0 ? -150 : 150 }, // Alternates direction
         {
           opacity: 1,
           x: 0,
           duration: 1,
+          ease: 'power2.out',
           scrollTrigger: {
             trigger: element, // Each content div is its own trigger
-            start: 'top 70%', // Animation starts when the div enters the viewport
+            start: 'top 80%', // Animation starts when the div enters the viewport
             end: 'top 20%', // Animation reverses when the div exits this range
             scrub: true, // Smooth animation tied to scroll
             toggleActions: 'play reverse play reverse', // Plays forward on scroll down, reverses on scroll up
