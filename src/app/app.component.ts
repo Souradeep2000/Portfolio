2f3e852a8d5ecb768604e948a338b802d1105436
souradeep gharami <souradeepgharami2000@gmail.com>
2025-01-10 16:21:26 +0530

    fixed navigation buttom error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ParticlesBackgroundComponent } from './particles-background/particles-background.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from "./footer/footer.component";
+import { FooterComponent } from './footer/footer.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ParticlesBackgroundComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ParticlesBackgroundComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Set scroll restoration behavior to 'manual' for custom handling
+    window.history.scrollRestoration = 'manual';
+
+    // Listen for navigation end event to reset scroll position
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo(0, 0); // Scroll to the top of the page
+      });
+  }
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -10,4 +10,5 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 })
 export class ResumeComponent {
   constructor() {}
+  
 }
